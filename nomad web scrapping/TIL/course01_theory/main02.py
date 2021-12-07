ad2a81cb5df6f968cb81a9f49ad2c0c74ab1cfa0
silversoren <ehsoren9@gmail.com>
2021-12-08 01:09:19 +0900

    #1.4 Creating a Your First Python Function

---
+++ nomad web scrapping/TIL/course01_theory/main02.py
@@ -0,0 +1,12 @@
+print("==============================")
+print("1.4 <Creating a Your First Python Function>")
+# 함수 정의하기
+# 파이썬에서는 function을 만든다고 하지 않고 function을 define(정의)한다고 한다.
+# function 정의하는 방식 : def라고 쓴 후 function 이름을 쓴다. 함수 내용은 :을 입력한 다음 줄에 들여쓰기 되어야 한다.
+
+def say_hello():
+    print("hello")
+    print("bye")
+
+# 함수 실행하기
+say_hello()
