5719b8c17e0c0806be9dff1e933e3f35aa4e1f7a
silversoren <ehsoren9@gmail.com>
2021-12-08 00:05:26 +0900

    #1.0 Data Types of Python

---
+++ nomad web scrapping/TIL/course01_theory/main01.py
@@ -0,0 +1,33 @@
+print("1.0 <Data Types of Python>")
+# 변수 variable
+# 변수는 수학과 비슷하다. a=2, b=3 처럼
+a=2
+b=3
+print(a+b)
+
+# data type 종류 - string, boolean, integer, float, none
+# (변수이름이 길어지면 snake case를 사용한다.)
+# 문자는 quote 로 감싸져 있어야 한다.
+a_string = "like this"  
+
+# boolean은 앞글자가 항상 대문자다.
+a_boolean = False
+
+# 숫자
+a_number = 3
+
+# 실수
+a_float = 3.12
+
+# none. 존재하지 않는다는 의미. 파이썬에만 있는 개념.
+a_none = None
+
+
+
+# data type 알아내는 메소드 
+print(type(a_number))
+print(type(a_boolean))
+print(type(a_string))
+print(type(a_none))  #NoneType
+
+
