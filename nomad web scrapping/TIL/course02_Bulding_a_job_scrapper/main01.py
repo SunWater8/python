8c920a79a327fa56c21aa1e6711e06381429ab25
silversoren <ehsoren9@gmail.com>
2021-12-12 20:32:18 +0900

    Update main01.py

--- nomad web scrapping/TIL/course02_Bulding_a_job_scrapper/main01.py
+++ nomad web scrapping/TIL/course02_Bulding_a_job_scrapper/main01.py
@@ -10,9 +10,31 @@ print("2.1 What are We Building")
 
 print("==============================")
 print("2.2 Navigating with Python")
-# 웹사이트 들어가기.
-# 몇개의 페이지가 있는지 확인하기
-# 페이지 하나씩 들어가보기
-# 상세검색으로 검색할 수도 있다.
+# indeed와 stackoverflow라는 구인구직사이트에서 검색 결과를 하나의 엑셀 시트로 가져오는 작업 해보기.
 
-# https://github.com/psf/requests
+"""
+1. 웹사이트 들어가기. (https://kr.indeed.com/)
+2. 몇개의 페이지가 있는지 확인하기
+3. 페이지 하나씩 들어가보기
+4. 상세검색으로 검색할 수도 있다.
+
+python requests 패키지 다운받아 import 하기.
+python requests 란? : 파이썬에서 요청을 만드는 기능을 모아 놓은 것
+
+ repl.it의 packages 다운 받는 곳에 들어가 requests: Python HTTP for humans를 선택하여 다운받기.
+ py파일에 import requests 라고 첫 줄에 입력하고 https://github.com/psf/requests 에서 관련 문장 가지고 오기.
+
+
+"""
+
+# indeed 사이트에서 python이라고 검색한 결과를 가져와보기. (검색 결과 개수와 검색 결과  html)
+import requests
+
+indeed_result = requests.get("https://kr.indeed.com/jobs?q=python&limit=50") # indeed에서 python을 검색했을 때 나오는 url
+
+print(indeed_result) # <Response [200]>
+# print(indeed_result.text) # 모든 html을 가져옴. 여기서 필요한 정보를 가지고 와야 한다. (필요한 정보: 페이지 숫자들... 이 정보는 beautifulsoup 이라는 곳에서 가져올 수 있다. --> https://www.crummy.com/software/BeautifulSoup/)
+"""
+beautiful soup 은 html에서 정보를 추출하기에 정말 유용한 package(라이브러리)이다. 이 package를 사용하기 위해서는 repl.it에서 다운을 받으면 된다.
+(beautifulsoup4:screen scrapping library 라고 검색한 것을 다운받기)
+"""
